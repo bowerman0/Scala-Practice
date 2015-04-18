7a40607a5dbfe67a017676e2af56a83b062b54be
Michael Bowerman <michael.bowerman@gmail.com>
2015-04-18 12:14:52 -0500

    FizzBuzz commit.

--- FizzBuzz.scala
+++ FizzBuzz.scala
@@ -1,10 +1,14 @@
+package com.example.FizzBuzz {
 object FizzBuzz {
   def main(args: Array[String]) {
-  fizzBuzz(1,100)
+   fizzBuzz(1,100)
   }
 
-  final def fizzBuzz(cur: Int, end: Int) {
-   if( cur < end ) {
+  final def fizzBuzz(cur: Int, end: Int) : Int = {
+   if( cur > end ) {
+    return 0
+   } 
+
    var isFizz = cur%3 == 0
    var isBuzz = cur%5 == 0
    if( isFizz ) {
@@ -14,13 +18,13 @@ object FizzBuzz {
    if( isBuzz ) {
     print("Buzz")
    }
-
+ 
    if( !isFizz && !isBuzz ) {
     print(cur)
    }
-
+ 
    println()
    fizzBuzz(cur+1,end)
-  } 
- }
+  }
+}
 }
