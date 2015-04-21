4dc5467248c17432313924a3e169e5d51e5259ab
Michael Bowerman <michael.bowerman@gmail.com>
2015-04-21 07:16:50 -0500

    First pass deploy.sh for war

--- run.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-if "%ServiceHostingSDKInstallPath%" == "" (
-    echo Can't see the ServiceHostingSDKInstallPath environment variable. Please run from a Windows Azure SDK command-line (run Program Files\Microsoft SDKs\Windows Azure\.NET SDK\^<version^>\bin\setenv.cmd^).
-    GOTO :eof
-)
-
-cspack ServiceDefinition.csdef /copyOnly /out:PackAndDeploy.csx
-csrun PackAndDeploy.csx ServiceConfiguration.Local.cscfg
-if "%ERRORLEVEL%"=="0" ( echo Browse to the port you see above to view the app, e.g. http://127.0.0.1:81. To stop the compute emulator, use "csrun /devfabric:shutdown" )
